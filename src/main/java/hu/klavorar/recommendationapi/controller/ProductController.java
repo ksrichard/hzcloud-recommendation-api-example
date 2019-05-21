84eded1db95c03c4565b6f82660581e039069676
ksrichard <richard.klavora@hazelcast.com>
2019-05-21 09:28:45 +0200

    added comments to Controller

--- src/main/java/hu/klavorar/recommendationapi/controller/ProductController.java
+++ src/main/java/hu/klavorar/recommendationapi/controller/ProductController.java
@@ -15,16 +15,32 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    /**
+     * Get all products endpoint
+     * @return all products
+     */
     @GetMapping
     public Flux<Product> getProducts() {
         return productService.getProducts();
     }
 
+    /**
+     * Get one product details by ID
+     * @param productId Product ID
+     * @param session User's HTTP Session
+     * @return Product details
+     */
     @GetMapping("/{productId}")
     public Mono<Product> getProduct(@PathVariable("productId") Long productId, WebSession session) {
         return productService.getProduct(session, productId);
     }
 
+    /**
+     * Get recommendations for a product
+     * @param productId Product ID
+     * @param limit Maximum number of product recommendations to show
+     * @return Recommended products
+     */
     @GetMapping("/{productId}/recommendations")
     public Flux<Product> getProductRecommendations(
             @PathVariable("productId") Long productId,
@@ -33,6 +49,12 @@ public class ProductController {
         return productService.getProductRecommendations(productId, limit);
     }
 
+    /**
+     * Get general recommendations for the current user's session
+     * @param limit Maximum number of product recommendations
+     * @param session User's HTTP session
+     * @return Recommended products
+     */
     @GetMapping("/recommendations")
     public Flux<Product> getRecommendations(
             @RequestParam(name = "limit", defaultValue= "10") Long limit,
